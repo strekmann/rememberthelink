2a47c356221324d0f3ed8676723c6b9c8a2a22b5
Jørgen Bergquist <gismos@gmail.com>
2013-07-18 15:19:33 +0200

    prefixable example

--- server/routes/example.js
+++ server/routes/example.js
@@ -1,6 +1,8 @@
 // example module routes
-module.exports = function(app) {
-    app.get('/', function(req, res){
+module.exports = function(app, prefix) {
+	prefix = prefix || '';
+
+    app.get(prefix+'/', function(req, res){
         res.render('example/index', {
             message: "hello node!"
         });
