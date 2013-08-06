f617d9ff3e0446f3361b60d8c7c3ae6d9bb22951
Sigurd Gartmann <sigurdga@sigurdga.no>
2013-08-06 23:26:00 +0200

    Add dateformat helper, and use it in links index

--- server/lib/helpers.js
+++ server/lib/helpers.js
@@ -1,7 +1,15 @@
 // Handlebars helpers
+var moment = require('moment');
 
 module.exports.register = function(hbs) {
 	hbs.registerHelper('example', function(text){
 		return "EXAMPLE: " + text;
 	});
+
+    //  http://momentjs.com/
+    //  usage: {{date creation_date format="MMMM YYYY"}}
+    hbs.registerHelper('dateformat', function(context, block) {
+        var f = block.hash.format || "YYYY-MM-DD HH:mm";
+        return moment(Date(context)).format(f);
+    });
 };
