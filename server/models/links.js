14f7f87d5dfc93c7a460adea2ea3b6a5b8a2808e
Sigurd Gartmann <sigurdga@sigurdga.no>
2013-08-27 09:46:33 +0200

    Add suggestion model

--- server/models/links.js
+++ server/models/links.js
@@ -12,10 +12,22 @@ var LinkSchema = new mongoose.Schema({
     created: {type: Date, required: true, 'default': Date.now}
 });
 
-LinkSchema.index({url: 1, creator: 1}, {unique: true});
+var SuggestionSchema = new mongoose.Schema({
+    url: {type: String, lowercase: true, trim: true, required: true},
+    title: {type: String},
+    description: {type: String},
+    from: {type: String, required: true, ref: 'User'},
+    to: {type: String, required: true, ref: 'User'},
+    created: {type: Date, required: true, 'default': Date.now}
+});
 
-var Link = mongoose.model('Link', LinkSchema);
+LinkSchema.index({url: 1, creator: 1}, {unique: true});
+SuggestionSchema.index({url: 1, from: 1, to: 1}, {unique: true});
+
+var Link = mongoose.model('Link', LinkSchema),
+    Suggestion = mongoose.model('Suggestion', SuggestionSchema);
 
 module.exports = {
-    Link: Link
+    Link: Link,
+    Suggestion: Suggestion
 };
