1ddfc2b21721578d0e6f4bb8ecabf1bcd0ea3dbf
Sigurd Gartmann <sigurdga@sigurdga.no>
2013-08-27 09:43:44 +0200

    Tags cannot be unique here, or you'll have only one link per tag

--- server/models/index.js
+++ server/models/index.js
@@ -18,7 +18,7 @@ var UserSchema = new mongoose.Schema({
 });
 
 var TagSchema = new mongoose.Schema({
-    _id: {type: String, lowercase: true, trim: true, required: true, unique: true}
+    _id: {type: String, lowercase: true, trim: true, required: true}
 });
 
 var User = mongoose.model('User', UserSchema);
