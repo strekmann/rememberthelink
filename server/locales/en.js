4bd77270c97f412abafd49c7d83e6d2bde4bc347
Jørgen Svedal Bergquist <gismos@gmail.com>
2013-08-29 22:15:06 +0200

    some translations

--- server/locales/en.js
+++ server/locales/en.js
@@ -1,4 +1,11 @@
 {
     "With blueberry jam": "With blueberry jam",
-    "hello world": "hello world"
+    "hello world": "hello world",
+    "Menu": "Menu",
+    "a place to store and share links": "a place to store and share links",
+    "Log in": "Log in",
+    "Suggestions": "Suggestions",
+    "Friends": "Friends",
+    "Account": "Account",
+    "Log out": "Log out"
 }
--- server/locales/nb.js
+++ server/locales/nb.js
@@ -1,4 +1,11 @@
 {
     "With blueberry jam": "Med blåbærsyltetøy",
-    "hello world": "heia verden"
+    "hello world": "heia verden",
+    "Menu": "Meny",
+    "a place to store and share links": "en plass man kan lagre og dele lenker",
+    "Log in": "Logg inn",
+    "Suggestions": "Forslag",
+    "Friends": "Venner",
+    "Account": "Konto",
+    "Log out": "Logg ut"
 }
