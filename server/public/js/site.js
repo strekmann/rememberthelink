c77e154027cb238aac382716e2b43fda380dad52
Jørgen Bergquist <gismos@gmail.com>
2013-09-03 22:32:26 +0200

    more link list styling, font-awesum, client-side js cleanup wip

--- server/public/js/site.js
+++ server/public/js/site.js
@@ -64,38 +64,78 @@
     $.fn.s7n = $.fn.s7n || {};
 
     $.fn.s7n.initLinks = function(options) {
-        $('input.private').on('click', function (ev) {
-            $(this).parent().parent().find('span.private').toggle();
+        // -- index page
+        // new link
+        $('form.new').on('submit', function(ev){
+            var form = $(this);
+            var input = form.find('input');
+
+            // empty field?
+            if ($.trim(input.val()) === '') {
+                input.addClass('error');
+                setTimeout(function(){
+                    input.removeClass('error');
+                }, 2000);
+                return false;
+            }
+
+            if (input.val().indexOf("://") === -1) {
+                input.val("http://" + input.val());
+            }
         });
 
-        $('a.delete').on('click', function (ev) {
-            ev.preventDefault();
+        // delete link
+        $('ul.links').delegate('a.delete', 'click', function(){
             var sure = $(this).parent().find('.sure').first();
             sure.show();
             return false;
         });
 
-        $('a.sure').on('click', function (ev) {
-            ev.preventDefault();
+        // comfirm delete
+        $('ul.links').delegate('a.sure', 'click', function(){
             var self = $(this);
-            var block = $(this).parent().parent();
-            var url = block.find('.link .title').first().attr('href');
-            block.hide();
+            var block = $(this).parents('li');
+            var url = block.find('.link .title a').first().attr('href');
+            self.hide();
+
             $.ajax({
                 method: 'DELETE',
                 url: '/delete',
                 data: {url: url},
                 success: function(data, status, xhr) {
                     if (status !== "success") {
-                        alert("Could not delete");
-                        self.hide();
-                        block.show();
+                        return alert("Could not delete");
+                    }
+                    block.remove();
+                }
+            });
+            return false;
+        });
+
+        // share link
+        $('ul.links').delegate('a.share', 'click', function(){
+            var block = $(this).parents('li');
+            var url = block.find('.link .title a').first().attr('href');
+            $.ajax({
+                method: 'GET',
+                url: '/friends/followers',
+                success: function(data, status, xhr) {
+                    var followers = "";
+                    if (status !== "success") {
+                        alert("Could not get followers");
+                    } else {
+                        $.each(data.followers, function(i, follower) {
+                            followers += '<form method="post" action="/share"><input type="hidden" name="id" value="' + follower._id + '"><input type="hidden" name="url" value="' + url + '"><button type="submit">' + follower.username + '</button></form>';
+                        });
+                        block.append('<div class="followers">' + followers + '</div>');
                     }
                 }
             });
             return false;
         });
 
+
+        // -- TODO: use delegate
         $('a.reject').on('click', function (ev) {
             ev.preventDefault();
             var self = $(this);
@@ -126,32 +166,7 @@
             link.hide();
         });
 
-        $('a.edit').on('click', function (ev) {
-            ev.preventDefault();
-            var block = $(this).parent().parent();
-            var form = block.find('form').first();
-            var link = block.find('.link').first();
-            form.show();
-            link.hide();
-        });
-
-        $('form.new').on('submit', function(ev){
-            var form = $(this);
-            var input = form.find('input');
-
-            // empty field?
-            if ($.trim(input.val()) === '') {
-                input.addClass('error');
-                setTimeout(function(){
-                    input.removeClass('error');
-                }, 2000);
-                return false;
-            }
-
-            if (input.val().indexOf("://") === -1) {
-                input.val("http://" + input.val());
-            }
-        });
+        
 
         $('form.suggestform').on('submit', function (){
             var block = $(this).parent().parent().parent();
@@ -181,26 +196,7 @@
             $(form).hide();
             $(link).show();
         });
-        $('a.share').on('click', function (ev) {
-            ev.preventDefault();
-            var block = $(this).parent().parent();
-            var url = block.find('.link .title').first().attr('href');
-            $.ajax({
-                method: 'GET',
-                url: '/friends/followers',
-                success: function(data, status, xhr) {
-                    var followers = "";
-                    if (status !== "success") {
-                        alert("Could not get followers");
-                    } else {
-                        $.each(data.followers, function(i, follower) {
-                            followers += '<form method="post" action="/share"><input type="hidden" name="id" value="' + follower._id + '"><input type="hidden" name="url" value="' + url + '"><button type="submit">' + follower.username + '</button></form>';
-                        });
-                        block.append('<div class="followers">' + followers + '</div>');
-                    }
-                }
-            });
-        });
+
 
         $('form.editform').on('submit', function() {
             var form = $(this);
