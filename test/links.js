5bff11fa721c0172b51421f4babd84a9388155c4
Sigurd Gartmann <sigurdga@sigurdga.no>
2013-10-23 00:22:16 +0200

    Check adding of link.
    Todo: check tags

--- test/links.js
+++ test/links.js
@@ -1,7 +1,7 @@
 describe("links", function(){
     var mongoose = require('mongoose'),
         cheerio = require('cheerio'),
-        routes = require('../server/routes/links'),
+        link_routes = require('../server/routes/links'),
         User = require('../server/models').User,
         user;
 
@@ -27,12 +27,12 @@ describe("links", function(){
         done();
     });
 
-    describe("fetch logged in frontpage", function(){
+    describe("fetch frontpage for logged in user", function(){
         it("should show index for logged in user", function(done){
             app.get('/test/', function(req, res){
                 req.user = user; // add test user to request
                 res.locals.user = user; // add user to templates
-                return routes.index(req, res);
+                return link_routes.index(req, res);
             });
 
             request(app)
@@ -46,5 +46,60 @@ describe("links", function(){
                     done();
                 });
         });
+    });
+
+    describe("save link", function () {
+        it("should let user save one link", function (done) {
+            app.get('/test/', function(req, res){
+                req.user = res.locals.user = user;
+                return link_routes.index(req, res);
+            });
+            app.get('/test/new', function (req, res) {
+                req.user = res.locals.user = user;
+                return link_routes.new_link(req, res);
+            });
+            app.post('/test/new', function (req, res) {
+                req.user = res.locals.user = user;
+                return link_routes.create_link(req, res);
+            });
+
+            request(app)
+                .get('/test/new?url=http://www.google.no/')
+                .set('Accept', 'text/html')
+                .expect(200)
+                .end(function(err, res){
+                    if (err) { return done(err); }
+                    $ = cheerio.load(res.text);
+                    var url = $('#url').first().val();
+                    var title = $('#title').first().val();
+                    title.should.equal('Google');  // not very smart
+
+                    request(app)
+                        .post('/test/new')
+                        .send({url: url, title: title, description: "Nothing to see here", tags: "search, find"})
+                        .set('Accept', 'text/html')
+                        .expect(302)
+                        .end(function (err, res) {
+                            if (err) { return done(err); }
+
+                            request(app)
+                                .get('/test/')
+                                .set('Accept', 'text/html')
+                                .expect(200)
+                                .end(function(err, res){
+                                    if (err) { return done(err); }
+                                    $ = cheerio.load(res.text);
+                                    var links = $('#links-index .links').children();
+                                    links.length.should.equal(1);
+                                    var first = links.first();
+                                    first.find('h2.title a').attr('href').should.equal('http://www.google.no/');
+                                    first.find('h2.title a').text().should.equal('Google');
+                                    first.find('.description').text().should.equal('Nothing to see here');
+                                    // TODO: Check tags
+                                    done();
+                                });
+                        });
+                });
+        })
     });
 });
