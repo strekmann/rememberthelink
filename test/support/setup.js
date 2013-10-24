948a9edb24d74dd1537de01639f880a89318029f
Jørgen Bergquist <gismos@gmail.com>
2013-10-24 18:31:04 +0200

    database setup moved to mocha setup file

--- test/support/setup.js
+++ test/support/setup.js
@@ -1,7 +1,11 @@
 var app = require('../../server/app.js');
+var mongoose = require('mongoose');
 var request = require('supertest');
 var chai = require('chai');
 chai.should();
+
+app.db = mongoose.connect('mongodb://localhost/' + app.conf.db_name + '_test');
+app.db.connection.db.dropDatabase();
 
 global.app = app;
 global.expect = chai.expect;
